5ea3e56c3bf5f9042c0e3ab78e7ec6977ae49dae
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-01 04:06:20 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,26 +1,40 @@
 
 
-def unsafe?(speed)
 
-  if speed > 60 || speed < 40 
-   return true 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def unsafe?(speed)
+
+#   if speed > 60 || speed < 40 
+#   return true 
   
-  else
-    return false
+#   else
+#     return false
   
-  end
-end
+#   end
+# end
 
 
 
 
-def not_safe?(speed)
+# def not_safe?(speed)
 
 
-  speed < 40 || speed > 60 ? true : false  
+#   speed < 40 || speed > 60 ? true : false  
     
-  #speed > 60 ? true : false  
+#   #speed > 60 ? true : false  
 
-end
+# end
 
 
